75f46b8c273dd5440bb34a8479050729a3dbd81b
Mike Yang <mky@astro.ncu.edu.tw>
2019-09-18 18:51:37 +0300

    convfli

--- exercise.py
+++ exercise.py
@@ -1,11 +1,7 @@
 
-<<<<<<< HEAD
 print("He added a lot of new features")
 print("He added a lot of new features")
 print("He added a lot of new features")
-print("He added a lot of new features")
-print("He added a lot of new features")
-=======
 print("This is the 2nd version made by Mike Yang")
 print("This is the 2nd version made by Mike Yang")
 print("This is the 2nd version made by Mike Yang")
@@ -13,8 +9,4 @@ print("This is the 2nd version made by Mike Yang")
 print("This is the 2nd version made by Mike Yang")
 print("This is the 2nd version made by Mike Yang")
 print("This is the 2nd version made by Mike Yang")
-print("This is the 2nd version made by Mike Yang")
-print("This is the 2nd version made by Mike Yang")
-print("This is the 2nd version made by Mike Yang")
->>>>>>> test1
 print("I added a lot of new features")
