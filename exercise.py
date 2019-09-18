5eb6726f96dc4da87feaa096496b125107d5cfeb
Mike Yang <mky@astro.ncu.edu.tw>
2019-09-18 18:35:08 +0300

    vonb

--- exercise.py
+++ exercise.py
@@ -1,3 +1,12 @@
 
 print("This is the 2nd version made by Mike Yang")
+print("This is the 2nd version made by Mike Yang")
+print("This is the 2nd version made by Mike Yang")
+print("This is the 2nd version made by Mike Yang")
+print("This is the 2nd version made by Mike Yang")
+print("This is the 2nd version made by Mike Yang")
+print("This is the 2nd version made by Mike Yang")
+print("This is the 2nd version made by Mike Yang")
+print("This is the 2nd version made by Mike Yang")
+print("This is the 2nd version made by Mike Yang")
 print("I added a lot of new features")
